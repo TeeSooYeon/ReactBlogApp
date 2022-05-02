96fdafad67079d45fd4e6c9c836e5946183eeb4f
hansludwig <hans1toquero@gmail.com>
2022-05-02 10:28:18 +0800

    changed the warning message in NotFound.js

--- src/NotFound.js
+++ src/NotFound.js
@@ -1,13 +1,13 @@
 import { Link } from "react-router-dom";
 
 const NotFound = () => {
-    return ( 
+    return (
         <div className="not-found">
             <h2>Sorry</h2>
-            <p>That page cannot be found maybe it does not exist.</p>
-            <Link to="/">Back to homepage...</Link>
+            <p>That page cannot be found maybe it does not exist. Please check again!</p>
+            <Link to="/">Go back to homepage...</Link>
         </div>
      );
 }
- 
+
 export default NotFound;
